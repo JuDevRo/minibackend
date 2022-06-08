32e2d128e6a6111ddb9fad3f8c6e276fd49077dd
root <root@LAPTOP-PPB2M666.localdomain>
2022-06-08 17:18:49 -0300

    Four Commit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      }
+    }),
+  );
+
+const config = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('MINI BACK')
+    .setVersion('1.0')
+    .build();
+const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.enableCors();
-  await app.listen(3005);
+  await app.listen( 3001);
 }
 bootstrap();
